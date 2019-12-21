38dbbb18137722c007cbd43508e06819729ec0d2
HeskeyBaozi <hezhiyu233@foxmail.com>
2019-12-21 14:29:09 +0800

    Initial Commit

---
+++ src/app.config.ts
@@ -0,0 +1,11 @@
+import { AppConfig } from "remax/wechat";
+
+const config: AppConfig = {
+  pages: ['pages/index/index'],
+  window: {
+    navigationBarTitleText: 'WildDream Lite',
+    navigationBarBackgroundColor: '#282c34'
+  }
+};
+
+export default config;
---
+++ src/pages/index/index.config.ts
@@ -0,0 +1,7 @@
+import { PageConfig } from "remax/wechat";
+
+const config: PageConfig = {
+  navigationBarTitleText: 'WildDream Lite Index'
+};
+
+export default config;
